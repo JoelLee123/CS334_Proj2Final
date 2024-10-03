734839588688d6381a2767eae8c02602a4d140db
Lara Jacobs <23260122@sun.ac.za>
2024-10-03 20:22:28 +0200

    need to implement some sort of filtering, the idea is to allow for different types of filtering with various options as given in the spec i.e., search by title, sort by last edited (ascending or descending as well as filter by categories...

--- frontend/src/frontend/pages/NotesPage.jsx
+++ frontend/src/frontend/pages/NotesPage.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import React, { useEffect }from 'react';
+import React, { useEffect } from 'react';
 import NoteCard from '../components/NoteCard';  // Adjust the import path as necessary
 
 
@@ -8,6 +8,8 @@ const NotesPage = () => {
 
   const [notes, setNotes] = useState([]);
   const [searchTitle, setSearch]=useState("");
+  const [categoryId, setCategoryId]=useState("");
+  const [categories, setCategories] = useState([]);
 
   const getNotes = async () => {
     try {
@@ -20,11 +22,11 @@ const NotesPage = () => {
       // const sampleNotes = data.notes;
 
       if (response.ok){
-        console.log("Note saved")
+        console.log("Note fetched")
         console.log(data.notes);
         setNotes(data.notes);
       } else {
-        console.log("Note not saved", data.message);
+        console.log("Note not fetched", data.message);
       }
         
     } catch (error) {
@@ -36,7 +38,6 @@ const NotesPage = () => {
     console.log({searchTitle});
   };
 
-
   useEffect(() => {
     getNotes();  // Call getNotes when component mounts
   }, []); 
@@ -45,19 +46,39 @@ const NotesPage = () => {
     <div className="bg-LighterBlue min-h-screen p-5">
       <h1 className="text-3xl font-bold text-DarkestBlue text-center mb-8">Your notes</h1>
       <div className="flex flex-col items-center">
-        <div classname ='mb-4 flex flex-col space-x-4' >
-      <input 
-          className="border border-DarkestBlue bg-Ivory rounded p-2 focus:outline-none focus:ring-2 focus:ring-blue-500 "
+      <div className="mb-4 flex items-center space-x-4">
+        <input
+          className="border border-DarkestBlue bg-Ivory rounded p-2 focus:outline-none focus:ring-2 focus:ring-blue-500 w-full"
           type="text"
           placeholder="Enter title"
           value={searchTitle}
           onChange={(e) => setSearch(e.target.value)}
         />
         <button
-        onClick={handleSearchTitle} 
-        className="bg-DarkestBlue  text-white rounded p-2 mb-4 focus:outline-none focus:ring-2 focus:ring-blue-500 "
-        >  search </button>
-        </div>
+          onClick={handleSearchTitle}
+          className="bg-DarkestBlue text-white rounded p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        >
+          Search
+        </button>
+        <select
+          className="border border-DarkestBlue bg-Ivory rounded p-2 focus:outline-none focus:ring-2 focus:ring-blue-500 w-full"
+          value={categoryId}
+          // onClick={getAllCategories}
+          onChange={(e) => setCategoryId(e.target.value)}
+        >
+          <option value="" disabled>
+            Filter by category
+          </option>
+          {categories.map((category, index) => (
+            <option 
+            key={index} value={category}
+            category = {category.category}>
+            <h3 className="text-xl font-bold text-DarkestBlue">{category.category}</h3>
+              {/* {category.name} */}
+            </option>
+          ))}
+        </select>
+      </div>
         {notes.length > 0 ? (
           notes.map((note, index) => (
             <NoteCard
