8d2f7506c06ab13386466319bdac98df49cc7a73
17854865 <17854865@sun.ac.za>
2024-10-14 19:48:37 +0200

    added test

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,11 @@
+import { act } from 'react';  // Import `act` from react
 import { render, screen } from '@testing-library/react';
-import App from './pages/App';
+import App from './frontend/pages/App'; // Adjust the path according to your project structure
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders the welcome message parts', () => {
+    render(<App />);
+  const welcomePart = screen.getByText(/Welcome to/i);
+  const scribeMarkPart = screen.getByText(/ScribeMark/i);
+  expect(welcomePart).toBeInTheDocument();
+  expect(scribeMarkPart).toBeInTheDocument();
 });
